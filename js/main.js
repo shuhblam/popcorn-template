244758fef2e77568bc7fd01b821956c354a5d4c8
cole <mcg42387@gmail.com>
2012-01-22 11:17:23 -0500

    jshint

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-  VideoJS.setupAllWhenReady();
-  var popcorn = Popcorn('#video'), bufferElement = document.getElementById("bufferText");
-  var update = function() {
-      if( popcorn.buffered().length > 0 ) {
-          bufferElement.innerHTML = popcorn.buffered().end(0);
-          if( popcorn.buffered().end( 0 ) > popcorn.duration() ) {
-             popcorn.play();
-          } else {
-             setTimeout( update, 100 );
-          } 
-      } else {
-          setTimeout( update, 100 );
-      }
-  };
-  update();
-});
+/*jshint undef:true */
+(function(){
+	"use strict";
+	VideoJS.setupAllWhenReady();
+	var popcorn = new Popcorn('#video'), bufferElement = document.getElementById("bufferText");
+	var update = function() {
+		if( popcorn.buffered().length > 0 ) {
+				bufferElement.innerHTML = popcorn.buffered().end(0);
+				if( popcorn.buffered().end( 0 ) > popcorn.duration() ) {
+					popcorn.play();
+				} else {
+					setTimeout( update, 100 );
+				}
+		} else {
+			setTimeout( update, 100 );
+		}
+	};
+	update();
+})(window);
+
